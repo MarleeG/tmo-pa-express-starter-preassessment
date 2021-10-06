e832d1925cae81904adecb56d04f65d079f9bece
Marlee Gerard <marlee.gerard8@t-mobile.com>
2021-10-06 10:52:52 -0400

    initial-commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+let books = require("./books.json");
+
+const { log } = console;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Don't panic.");
+});
+
+app.get("/api/books", (req, res) => {
+  const sortedBooks = books.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()))
+
+  books.length > 0 ?
+    res.status(200).json({ books: sortedBooks }) :
+    res.status(200).json({ "message": "There aren't any books available in your library at this time." })
+});
+
+app.post("/api/books", (req, res) => {
+  const book = { id: uuidv4(), ...req.body };
+
+  books = [book, ...books];
+
+  book.hasOwnProperty('author') ?
+    res.status(201).json(book) :
+    res.status(500).json({ error: "An error has occured. Please ensure your book includes an author property in your json body. " });
+});
+
+app.delete("/api/books", (req, res) => {
+  if (books.length > 0) books = [];
+  res.status(204).end()
+})
+
+
+module.exports = app;
